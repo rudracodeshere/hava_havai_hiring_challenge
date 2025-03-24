ecec28f667798c60e5fed970962ad5049e15c0ab
Rudra Sharma <rudracollegeclass@gmail.com>
2025-03-24 22:31:37 +0530

    Initialize Flutter project with iOS and Android configurations, assets, and basic app structure

---
+++ android/app/src/main/kotlin/com/example/hava_havai_hiring_challenge/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.hava_havai_hiring_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Home());
+}
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
